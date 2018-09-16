41e7377c318d4952bb8f8c4e66947f41b36484c0
Caitlin Pretorius <30002731@student.toiohomai.ac.nz>
2018-09-17 11:24:47 +1200

    Dictionary

---
+++ Exercise 1/Exercise 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double > fruit = new Dictionary<string, double>()
+            {
+                {"Apple", 3.60},
+                {"Banana", 4.80},
+                {"Pear", 4.80},
+                {"Plum", 6.90},
+                {"Mango", 9.34},
+
+            };
+
+            Console.WriteLine(fruit["Apple"]);
+            Console.WriteLine(fruit["Banana"]);
+            Console.WriteLine(fruit["Pear"]);
+            Console.WriteLine(fruit["Plum"]);
+            Console.WriteLine(fruit["Mango"]);
+        }
+    }
+}
---
+++ Exercise 2/Exercise 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> user = new Dictionary<int, string>()
+            {
+                {1, ""},
+                {2, ""},
+                {3, ""},
+                {4, ""},
+                {5, ""}
+            };
+
+            for (int i = 0; i < user.Count; i++)
+            {   
+                Console.WriteLine("Key : {0} Value: {1}",
+                    user.Keys.ElementAt(i),
+                    user[user.Keys.ElementAt(i)]);
+            }
+        }
+    }
+}
